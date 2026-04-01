89e495e8722a8b4dba3184bd39591b34b8e8d0c5
M5tar599 <161099966+M5tar599@users.noreply.github.com>
2026-04-01 04:11:58 -0500

    Update path to sudoku.lp in load method

--- sudoku1.py
+++ sudoku1.py
@@ -7,7 +7,7 @@ class SudokuApp(Application):
     version = "1.0"
 
     def main(self, control, files):
-        control.load("sudoku.lp")
+        control.load("solutions/sudoku.lp")
         for f in files:
             control.load(f)
         control.ground([("base", [])])
